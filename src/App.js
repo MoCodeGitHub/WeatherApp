024debf11f139c03cb06bca79fc4c81fc20c818b
MoCodeGitHub <magical_sunrise_44@hotmail.com>
2020-12-10 02:40:09 +0100

    added search-box

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+const api = {
+key: "71a7647154d9132a86ef428e3f44c703",
+base: "https://api.openweathermap.org/data/2.5"
+}
 
 function App() {
   return (
     <div className="App">
-  
+  <main>
+    <div className=".search-box">
+      <input 
+        type="text"
+        className="search-bar"
+        placeholder="Search..."
+
+        />
+    </div>
+  </main>
     </div>
   );
 }
